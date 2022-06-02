c78218f4dd98d092861bb16bba7f5428d213714f
BenZr2 <52921668+BenZr2@users.noreply.github.com>
2022-06-02 10:26:32 +0200

    02.06.22

--- login.php
+++ login.php
@@ -1,8 +1,45 @@
+<?php
+        $servername = "192.168.200.3";
+        $username = "web";
+        $password = "dj39dn6j2";
+        $dbname = "website";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        //Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+
+        if (isset($_POST['username'])) {
+            $sql = "SELECT username, passwort FROM user";
+            $result = $conn->query($sql);
+
+                        $passwort = hash('sha256',$_POST['passwort']);
+
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    #echo "Username: " . $row["username"]. "| Passwort: " . $row["passwort"]. "<br>";
+
+
+
+                    if ($_POST['username'] == $row["username"] && $passwort == $row["passwort"]) {
+                        #echo "Erfolgreich angemeldet";
+                        header("Location: index.php");
+                        exit();
+                    }
+                }
+            }
+            echo "Falscher Benutzername oder falsches Passwort";
+            $conn->close();
+        }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -15,41 +52,5 @@
         <button type="submit">Anmelden</button>
     </form>
 
-
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "website";
-        
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        
-        //Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-
-        if (isset($_POST['username'])) {
-            $sql = "SELECT username, passwort FROM user";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-            // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    #echo "Username: " . $row["username"]. "| Passwort: " . $row["passwort"]. "<br>";
-                    if ($_POST['username'] == $row["username"] && $_POST['passwort'] == $row["passwort"]) {
-                        echo "Erfolgreich angemeldet";
-                        header("Location: startseite.html");
-                        exit();
-                    }
-                }
-            } 
-            echo "Falscher Benutzername oder falsches Passwort";
-            $conn->close();
-        }
-    ?>
-
-    <p><a href="register.php">Register</a></p>
-</body> 
-
+<p><a href="register.php">Register</a></p>
 </html>
