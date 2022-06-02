db8316eb4e072db52d6f9249692cba9a47d04054
BenZr2 <52921668+BenZr2@users.noreply.github.com>
2022-06-02 10:26:00 +0200

    02.06.22

--- register.php
+++ register.php
@@ -1,3 +1,68 @@
+<?php
+        $servername = "192.168.200.3";
+        $username = "web";
+        $password = "dj39dn6j2";
+        $dbname = "website";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+
+        if (isset($_POST['username'])) {
+
+            $sql_insert = $conn->prepare("INSERT INTO user (username, passwort) VALUES (?,?)");
+
+                        $passwort=hash('sha256',$_POST['passwort'] ) ;
+
+
+
+            $sql_insert->bind_param("ss", $_POST['username'],$passwort);
+
+
+            $sql_select = "SELECT username, passwort FROM user";
+                $result = $conn->query($sql_select);
+
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    #echo "Username: " . $row["username"]. "| Passwort: " . $row["passwort"]. "<br>";
+                    if ($_POST['username'] == $row["username"]) {
+                        echo "Sie sind bereits registriert";
+                        sleep(1);
+                        return;
+                    }
+                }
+
+
+            if ($sql_insert->execute() === TRUE) {
+                echo "Erfolgreich registriert" . "<br>";
+                sleep(1);
+                return;
+
+            } else {
+                echo "Fehler bei der Registrierung: " . $sql_insert . "<br>" . $conn->error;
+            }
+        }
+
+
+            $sql_select = "SELECT username, passwort FROM user";
+            $result = $conn->query($sql_select);
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<br>" . "Username: " . $row["username"]. " | Passwort: " . $row["passwort"]. "<br>";
+                }
+            }
+            $conn->close();
+        }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,68 +76,11 @@
         <input type="text" name="username" placeholder="Username">
         <input type="password" name="passwort" placeholder="Passwort">
         <input type="password" name="passwort" placeholder="Passwort wiederholen">
-        <button type="submit">Anmelden</button>
+        <button type="submit">Registrieren</button>
 
         <div>
             <p><a href="login.php">Login</a></p>
         </div>
     </form>
-
-
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "website";
-        
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-
-        if (isset($_POST['username'])) {
-            $sql_insert = $conn->prepare("INSERT INTO user (username, passwort) VALUES (?,?)");
-            $sql_insert->bind_param("ss", $_POST['username'], $_POST['passwort']);
-            
-            $sql_select = "SELECT username, passwort FROM user";
-            $result = $conn->query($sql_select);
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    #echo "Username: " . $row["username"]. "| Passwort: " . $row["passwort"]. "<br>";
-                    if ($_POST['username'] == $row["username"]) {
-                        echo "Sie sind bereits registriert";
-                        sleep(1);
-                        return;
-                    }
-                }
-
-            if ($sql_insert->execute() === TRUE) {
-                echo "Erfolgreich registriert" . "<br>";
-                sleep(1);
-                return;
-
-            } else {
-                echo "Fehler bei der Registrierung: " . $sql_insert . "<br>" . $conn->error;
-            }
-        } 
-            
-            
-            $sql_select = "SELECT username, passwort FROM user";
-            $result = $conn->query($sql_select);
-            
-            if ($result->num_rows > 0) {
-            // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    echo "<br>" . "Username: " . $row["username"]. " | Passwort: " . $row["passwort"]. "<br>";
-                }
-            } 
-            $conn->close();
-        }
-    ?>
-    
-    
 </body>
 </html>
