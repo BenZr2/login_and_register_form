b862524842734c73ce6f3acdbe36317bd259a8c7
BenZr2 <52921668+BenZr2@users.noreply.github.com>
2022-06-30 07:54:21 +0200

    Update artikel_search.php

--- artikel_search.php
+++ artikel_search.php
@@ -1,44 +1,60 @@
 <?php
-        $servername = "192.168.200.3";
-        $username = "web";
-        $password = "dj39dn6j2";
-        $dbname = "website";
+//server credentials
+$servername = "192.168.200.3";
+$username = "web";
+$password = "dj39dn6j2";
+$dbname = "website";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        //Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
+//Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+
+if(isset($_POST['search'])) {
+	$search = $_POST['search'];
+	$sql = "SELECT * FROM  Artikel WHERE ArtikelNR = $search";
+	//$statement = $conn->prepare($sql);
+	//$statement->bind_param('sss', $search, $search, $search);
+	$result = $conn->query($sql);
+
+	//print_r($result);
+
+	//print_r($result->num_rows);
+
+	//output results with while loop
+	if ($result->num_rows > 0) {
+		foreach($result as $row) {
+                //if ($_POST['search'] == $row["ArtikelNR"]) {
+                echo $row["ArtikelNR"];
+		echo "   ";
+		echo $row["Artikelname"];
+                echo "<br>";
+        //}
         }
-
-	if(isset($_POST['search'])) {
-	  $sql = "SELECT * FROM Artikel";
-          $result = $conn->query($sql);
-
-	while($row = $result->fetch_assoc()) {
-          if ($_POST['search'] == $row["ArtikelNR"]) {
-        	echo $row["ArtikelNR"];
-        	exit();
-           }
-         }
-            $conn->close();
-        }
+	}
+	exit();
+}
+$conn->close();
 ?>
 
 
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Search</title>
 </head>
 
 <body>
-    <form method="POST" action="">
-        <input type="text" name="search" placeholder="ArtikelNr">
-        <button type="submit">Suche</button>
-    </form>
+<h2>Artikel Suche</h2>
+<form method="POST" action="">
+<input type="text" name="search" placeholder="ArtikelNr, Artikelname, Marke">
+<button type="submit">Suche</button>
+</form>
 </html>
