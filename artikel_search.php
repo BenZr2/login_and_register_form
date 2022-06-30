d468a1bf71df8881167dba3a54d487368593cbd8
BenZr2 <52921668+BenZr2@users.noreply.github.com>
2022-06-30 08:17:42 +0200

    Update artikel_search.php

--- artikel_search.php
+++ artikel_search.php
@@ -1,60 +1,56 @@
 <?php
-//server credentials
+//Anmeldedaten für die Verbindung zum MySQL Server
 $servername = "192.168.200.3";
 $username = "web";
 $password = "dj39dn6j2";
 $dbname = "website";
-
+//Verbindungsaufbau zur Datebank
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-//Check connection
+//Überprüfung der Verbindung zur Datenbank und ggf. Error Handling
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 
-
+//String aus dem Eingabe Feld wird in SQL Befehl eingefügt und auf die Datenbank angewendet
 if(isset($_POST['search'])) {
 	$search = $_POST['search'];
 	$sql = "SELECT * FROM  Artikel WHERE ArtikelNR = $search";
-	//$statement = $conn->prepare($sql);
-	//$statement->bind_param('sss', $search, $search, $search);
 	$result = $conn->query($sql);
 
-	//print_r($result);
-
-	//print_r($result->num_rows);
-
-	//output results with while loop
+	//Wenn mindestens eine Zeile als Output zurückkommt, werden alle über eine for-Schleife nacheinander ausgegeben
 	if ($result->num_rows > 0) {
 		foreach($result as $row) {
-                //if ($_POST['search'] == $row["ArtikelNR"]) {
                 echo $row["ArtikelNR"];
 		echo "   ";
 		echo $row["Artikelname"];
                 echo "<br>";
-        //}
         }
 	}
-	exit();
+	exit();	
 }
-$conn->close();
+$conn->close(); //Schließt die Verbindung zur Datenbank
 ?>
 
 
 
 
 <!DOCTYPE html>
-<html lang="de">
-<head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Search</title>
-</head>
+	<html lang="de">
+	<!-- Metadaten der HTML Datei -->
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Artikel Suche</title>
+	</head>
 
-<body>
-<h2>Artikel Suche</h2>
-<form method="POST" action="">
-<input type="text" name="search" placeholder="ArtikelNr, Artikelname, Marke">
-<button type="submit">Suche</button>
-</form>
+	<body>
+		<h2>Artikel Suche</h2>
+
+		<!-- HTML Form zur Eingabe der Artikelnummer die gesucht werden soll -->
+		<form method="POST" action="">
+		<input type="text" name="search" placeholder="ArtikelNr, Artikelname, Marke">
+		<button type="submit">Suche</button>
+		</form>
+	</body>
 </html>
